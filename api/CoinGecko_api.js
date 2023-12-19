1ec70febc9e673d3a20088a857db3c0932396a05
Muhammed Mustafa Kapıcı <ug.mustafa.kapici@toros.edu.tr>
2023-12-19 04:04:30 +0300

    link updated

--- api/CoinGecko_api.js
+++ api/CoinGecko_api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchCoins = async () => {
   try {
-    const response = await axios.get('https://api-demo-production.up.railway.app/list');
+    const response = await axios.get('https://ailayzer.up.railway.app/list');
     return response.data;
   } catch (error) {
     console.error(error);
